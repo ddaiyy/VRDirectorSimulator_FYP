4e087bcddcee599e53760b26d080ac549156d418
zbaiyy01 <yuyan.zhang01@outlook.com>
2025-07-29 13:45:32 +0100

    VR键盘输入

---
+++ Assets/Scripts/UI/User/OpenKeyboardTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class OpenKeyboardTrigger : MonoBehaviour
+{
+    public VRKeyboardManager keyboardManager;
+    private TMP_InputField inputField;
+
+    private void Start()
+    {
+        inputField = GetComponent<TMP_InputField>();
+    }
+
+    public void OnClickInputField()
+    {
+        if (inputField == null)
+        {
+            Debug.LogError("❌ TMP_InputField 没挂！");
+            return;
+        }
+
+        keyboardManager.ShowKeyboard(inputField);
+    }
+}
